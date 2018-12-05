1922fc28f75a58400b60996c671788274bee4bbb
Manngold <jsw4820@gmail.com>
2018-12-05 17:26:03 +0900

    create clock

---
+++ clock.js
@@ -0,0 +1,19 @@
+const clock = document.querySelector(".js-clock"),
+  clockTitle = document.querySelector(".clock");
+
+function showingClock() {
+  const date = new Date(); //setInterval로 초기화를 해주면서 시간 데이터를 받아야해서 함수 안에다 선언함
+  const Hour = date.getHours(),
+    Minute = date.getMinutes(),
+    Second = date.getSeconds();
+  clockTitle.innerHTML = `${Hour < 10 ? `0${Hour}` : Hour} : ${
+    Minute < 10 ? `0${Minute}` : Minute
+  } : ${Second < 10 ? `0${Second}` : Second}`;
+}
+
+function init() {
+  showingClock();
+  setInterval(showingClock, 1000);
+}
+
+init();
