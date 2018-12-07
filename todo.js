c3f8d3451f9a6000c7229a83ea912aa10a0d58d9
Manngold <jsw4820@gmail.com>
2018-12-07 22:36:21 +0900

    add todo at localstorage

--- todo.js
+++ todo.js
@@ -5,15 +5,27 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = "toDos";
 const toDos = [];
 
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
   delBtn.innerText = "❌";
   span.innerHTML = text;
   li.appendChild(delBtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -24,9 +36,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
-  if (toDos !== null) {
-    paintToDo(toDo.text);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(toDo => {
+      paintToDo(toDo.text);
+    });
   }
 }
 
